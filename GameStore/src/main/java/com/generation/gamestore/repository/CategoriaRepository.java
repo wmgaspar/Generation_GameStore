1e6dcdd5cb2f2e4a19e559b6ce9529e84ae59b86
unknown <wesgaspar1@gmail.com>
2023-05-22 19:23:17 -0300

    first commit

---
+++ GameStore/src/main/java/com/generation/gamestore/repository/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.generation.gamestore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.gamestore.model.Categoria;
+
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	List<Categoria> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
---
+++ GameStore/src/main/java/com/generation/gamestore/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.generation.gamestore.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.gamestore.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+
+	List<Produto> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
